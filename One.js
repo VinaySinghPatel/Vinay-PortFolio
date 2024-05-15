34c1480e008524e13eaab30d3e161edcfc03e086
Vinay Singh Patel <vinaypatel898944@gmail.com>
2024-05-16 02:38:04 +0530

    Add files via upload

---
+++ One.js
@@ -0,0 +1,7 @@
+
+const ChangeTyping = new ChangeTyping(".TYPING",{
+    String : ["Java Developer","Frontend Developer","JS Developer","React Developer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true
+});
